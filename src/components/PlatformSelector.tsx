31ac41235406fdf57cbbf729d3cf36ffb6db023a
afragerami <afragerami@gmail.com>
2023-07-17 12:26:02 +0330

    Build Platform selector

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,67 @@
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatforms";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { Text, Icon, HStack } from "@chakra-ui/react";
+
+import { IconType } from "react-icons";
+
+export const PlatformSelector = () => {
+  const { data, error } = usePlatforms();
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  if (error) return null;
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Platforms
+      </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <>
+            <MenuItem key={platform.id}>
+              <Icon
+                key={platform.id}
+                as={iconMap[platform.slug]}
+                color="gray.500"
+                boxSize="24px"
+                padding="4px"
+              />
+              {platform.name}
+            </MenuItem>
+          </>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
